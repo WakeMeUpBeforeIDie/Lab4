c0ce7a46719e28cdb4334159d8aff2b675dc0021
AutumnAsh <aleksandr.kasyanov1337@mail.ru>
2026-04-11 13:06:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Stateless;
+namespace BugPro
+{
+    public enum StatesOfBug
+    {
+        Opened,
+        BeingAnalysed,
+        InProcess,
+        Postponed,
+        Solved,
+        Rejected,
+        Returning
+    }
+    public enum BugActions
+    {
+        Beginning,
+        Accept,
+        Do,
+        Reject,
+        TryToSolve,
+        WasOk,
+        WasBad,
+        Return
+    }
+    public class Bug
+    {
+        StateMachine<StatesOfBug, BugActions> sm;
+        public Bug()
+        {
+            sm = new StateMachine<StatesOfBug, BugActions>(StatesOfBug.Opened);
+            sm.Configure(StatesOfBug.Opened).Permit(BugActions.Beginning, StatesOfBug.BeingAnalysed);
+            sm.Configure(StatesOfBug.BeingAnalysed).Permit(BugActions.Accept, StatesOfBug.InProcess);
+            sm.Configure(StatesOfBug.BeingAnalysed).Permit(BugActions.Do, StatesOfBug.Postponed);
+            sm.Configure(StatesOfBug.BeingAnalysed).Permit(BugActions.Reject, StatesOfBug.Rejected);
+            sm.Configure(StatesOfBug.BeingAnalysed).Permit(BugActions.WasOk, StatesOfBug.Solved);
+            sm.Configure(StatesOfBug.BeingAnalysed).Permit(BugActions.WasBad, StatesOfBug.Returning);
+            sm.Configure(StatesOfBug.Postponed).Permit(BugActions.Beginning, StatesOfBug.BeingAnalysed);
+            sm.Configure(StatesOfBug.InProcess).Permit(BugActions.TryToSolve, StatesOfBug.BeingAnalysed);
+            sm.Configure(StatesOfBug.Returning).Permit(BugActions.Accept, StatesOfBug.InProcess);
+            sm.Configure(StatesOfBug.Returning).Permit(BugActions.Reject, StatesOfBug.Rejected);
+            sm.Configure(StatesOfBug.Rejected).Permit(BugActions.Return, StatesOfBug.BeingAnalysed);
+            sm.Configure(StatesOfBug.Solved).Permit(BugActions.Accept, StatesOfBug.Returning); 
+        }
+        public StatesOfBug GetState()
+        {
+            return sm.State;
+        }
+        public void Run()
+        {
+            sm.Fire(BugActions.Beginning);
+            Console.WriteLine("Machine has been started");
+        }
+        public void Accept()
+        {
+            sm.Fire(BugActions.Accept);
+            Console.WriteLine("Accepted");
+        }
+        public void Solve()
+        {
+            sm.Fire(BugActions.TryToSolve);
+            Console.WriteLine("Machine is solving");
+        }
+        public void Do()
+        {
+            sm.Fire(BugActions.Do);
+            Console.WriteLine("Machine is doing smth");
+        }
+        public void Reject()
+        {
+            sm.Fire(BugActions.Reject);
+            Console.WriteLine("Machine rejected");
+        }
+        public void Accomplished()
+        {
+            sm.Fire(BugActions.TryToSolve);
+            Console.WriteLine("Machine solved");
+        }
+        public void WhetherOk()
+        {
+            sm.Fire(BugActions.WasOk);
+            Console.WriteLine("OK");
+        }
+        public void WhetherBad()
+        {
+            sm.Fire(BugActions.WasBad);
+            Console.WriteLine("Not OK");
+        }
+        public void Return()
+        {
+            sm.Fire(BugActions.Return);
+            Console.WriteLine("Machine returned");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bug bug = new Bug();
+            bug.Run(); bug.Accept(); bug.Accomplished();
+            bug.WhetherBad(); bug.Accept(); bug.Accomplished();
+            bug.WhetherOk();
+            Console.WriteLine(bug.GetState());      
+        }
+    }
+}
---
+++ Test1.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BugPro;
+using System;
+namespace BugTests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Bug b = new Bug();
+            Assert.AreEqual(StatesOfBug.Opened, b.GetState());
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Bug b = new Bug();
+            b.Run();
+            Assert.AreEqual(StatesOfBug.BeingAnalysed, b.GetState());
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Bug b = new Bug();
+            b.Run(); b.Accept();
+            Assert.AreEqual(StatesOfBug.InProcess, b.GetState());
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Bug b = new Bug();
+            b.Run(); b.Reject();
+            Assert.AreEqual(StatesOfBug.Rejected, b.GetState());
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Bug b = new Bug();
+            b.Run(); b.WhetherOk();
+            Assert.AreEqual(StatesOfBug.Solved, b.GetState());
+        }
+        [TestMethod]
+        public void TestMetho6()
+        {
+            Bug b = new Bug();
+            b.Run(); b.WhetherBad();
+            Assert.AreEqual(StatesOfBug.Returning, b.GetState());
+        }
+        [TestMethod]
+        public void TestMethod7()
+        {
+            Bug b = new Bug();
+            b.Run(); b.Do();
+            Assert.AreEqual(StatesOfBug.Postponed, b.GetState());
+        }
+        [TestMethod]
+        public void TestMethod8()
+        {
+            Bug b = new Bug();
+            b.Run(); b.Do(); b.Run();
+            Assert.AreEqual(StatesOfBug.BeingAnalysed, b.GetState());
+        }
+        [TestMethod]
+        public void TestMethod9()
+        {
+            Bug b = new Bug();
+            b.Run(); b.Accept(); b.Accomplished();
+            Assert.AreEqual(StatesOfBug.BeingAnalysed, b.GetState());
+        }
+        [TestMethod]
+        public void TestMethod10()
+        {
+            Bug b = new Bug();
+            b.Run(); b.WhetherBad(); b.Accept();
+            Assert.AreEqual(StatesOfBug.InProcess, b.GetState());
+        }
+    }
+}
